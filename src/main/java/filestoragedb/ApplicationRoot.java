29204c4450ca03cd0cc54b1ab6cdf57abf7188ae
AndChelp <61141148+AndChelp@users.noreply.github.com>
2020-08-23 14:59:25 +0500

    Update ApplicationRoot.java

--- src/main/java/filestoragedb/ApplicationRoot.java
+++ src/main/java/filestoragedb/ApplicationRoot.java
@@ -20,7 +20,7 @@ public class ApplicationRoot {
     public Docket swagger2ApiConfig() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build();
     }
